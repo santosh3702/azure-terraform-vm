baa01c740a084f9c277ebc8421809463b7bca9c2
santosh3702 <santoshrahul81@gmail.com>
2019-10-28 21:21:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_public_ip" "pip" {
   name                         = "${var.prefix}-ip"
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.main.name}"
-  allocation_method            = "dynamic"
+  allocation_method            = "${var.pip_allocation_method}"
   domain_name_label            = "${var.hostname}"
 }
 
