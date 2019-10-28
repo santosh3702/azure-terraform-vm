1ed3ac9cc2a92b2a4aa5b780d8f9756b47202f49
santosh3702 <santoshrahul81@gmail.com>
2019-10-28 20:31:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,65 +1,102 @@
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
-  location = "West US 2"
+  location = "${var.location}"
 }
 
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = "${azurerm_resource_group.main.location}"
+  address_space       = ["${var.vnet_address_space}"]
+  location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
 }
 
 resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+  name                 = "${var.prefix}-subnet-internal"
   resource_group_name  = "${azurerm_resource_group.main.name}"
   virtual_network_name = "${azurerm_virtual_network.main.name}"
-  address_prefix       = "10.0.2.0/24"
+  address_prefix       = "${var.subnet_address_space}"
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  location            = "${azurerm_resource_group.main.location}"
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${var.prefix}-nsg"
+  location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
+}
+
+resource "azurerm_network_security_rule" "rulessh" {
+  name                        = "${var.prefix}-rulessh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.main.name}"
+  network_security_group_name = "${azurerm_network_security_group.nsg.name}"
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                         = "${var.prefix}-ip"
+  location                     = "${var.location}"
+  resource_group_name          = "${azurerm_resource_group.main.name}"
+  public_ip_address_allocation = "dynamic"
+  domain_name_label            = "${var.hostname}"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                      = "${var.prefix}-nic"
+  location                  = "${var.location}"
+  resource_group_name       = "${azurerm_resource_group.main.name}"
+  network_security_group_id = "${azurerm_network_security_group.nsg.id}"
 
   ip_configuration {
-    name                          = "testconfiguration1"
+    name                          = "${var.prefix}-ipconfig"
     subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.pip.id}"
   }
+
+  depends_on = ["azurerm_network_security_group.nsg"]
 }
+
 
 resource "azurerm_virtual_machine" "ibm-mq" {
   name                  = "${var.prefix}-vm"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
   network_interface_ids = ["${azurerm_network_interface.main.id}"]
-  vm_size               = "Standard_DS1_v2"
+  vm_size               = "${var.vm_size}"
 
   # This means the OS Disk will be deleted when Terraform destroys the Virtual Machine
   # NOTE: This may not be optimal in all cases.
   delete_os_disk_on_termination = true
-
+  
    storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
+    publisher = "${var.image_publisher}"
+    offer     = "${var.image_offer}"
+    sku       = "${var.image_sku}"
+    version   = "${var.image_version}"
   }
+  
   storage_os_disk {
     name              = "myosdisk1"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
+  
   os_profile {
-    computer_name  = "${local.virtual_machine_name}"
-    admin_username = "${local.admin_username}"
-    admin_password = "${local.admin_password}"
+    computer_name  = "${var.hostname}"
+    admin_username = "${var.admin_username}"
+    admin_password = "${var.admin_password}"
   }
+  
   os_profile_linux_config {
-    disable_password_authentication = false
+    disable_password_authentication = "${var.disable_password_authentication}"
   }
+  
   tags = {
     environment = "staging"
   }
