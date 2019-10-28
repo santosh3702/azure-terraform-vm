45f2bbdd38878497badec027ab6662fff535d585
santosh3702 <santoshrahul81@gmail.com>
2019-10-28 20:54:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,11 +10,75 @@ variable "azure_client_secret" {
 variable "azure_tenant_id" {
   type = "string"
 }
-variable "name_prefix" {
+variable "prefix" {
   description = "unique part of the name to give to resources"
   default     = "prd-maersk"
 }
 variable "location" {
   description = "region where the resources should exist"
   default     = "eastus"
+}
+
+# required variables
+variable "hostname" {
+  description = "name of the machine to create"
+}
+
+variable "ssh_public_key" {
+  description = "public key for ssh access"
+}
+
+variable "vnet_address_space" {
+  description = "full address space allowed to the virtual network"
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_address_space" {
+  description = "the subset of the virtual network for this subnet"
+  default     = "10.0.10.0/24"
+}
+
+variable "storage_account_type" {
+  description = "type of storage account"
+  default     = "Standard_LRS"
+}
+
+variable "vm_size" {
+  description = "size of the vm to create"
+  default     = "Standard_A0"
+}
+
+variable "image_publisher" {
+  description = "name of the publisher of the image (az vm image list)"
+  default     = "RedHat"
+}
+
+variable "image_offer" {
+  description = "the name of the offer (az vm image list)"
+  default     = "RHEL"
+}
+
+variable "image_sku" {
+  description = "image sku to apply (az vm image list)"
+  default     = "7-LVM"
+}
+
+variable "image_version" {
+  description = "version of the image to apply (az vm image list)"
+  default     = "latest"
+}
+
+variable "admin_username" {
+  description = "administrator user name"
+  default     = "vmadmin"
+}
+
+variable "admin_password" {
+  description = "administrator password (recommended to disable password auth)"
+  default     = "notused"
+}
+
+variable "disable_password_authentication" {
+  description = "toggle for password auth (recommended to keep disabled)"
+  default     = false
 }
