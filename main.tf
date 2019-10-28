2b3fd2870c2086623b659f6f160e676626526770
santosh3702 <santoshrahul81@gmail.com>
2019-10-28 19:43:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "example" {
+resource "azurerm_virtual_machine" "ibm-mq" {
   name                  = "${var.prefix}-vm"
   location              = "${azurerm_resource_group.main.location}"
   resource_group_name   = "${azurerm_resource_group.main.name}"
